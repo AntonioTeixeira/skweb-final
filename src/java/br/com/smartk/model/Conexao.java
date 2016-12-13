923046e05a47602607c50a852c6b2e64590d7d6c
Antonio Teixeira <antoniodepg@gmail.com>
2016-12-13 01:00:23 -0200

    Trocando pela segunda veez o título para verificar funcionamento

--- src/java/br/com/smartk/model/Conexao.java
+++ src/java/br/com/smartk/model/Conexao.java
@@ -17,10 +17,14 @@ public class Conexao {
         
     String drive = "com.mysql.jdbc.Driver";
     //String drive = "com.mysql.jdcb.Driver";    
-    String url = "jdbc:mysql://localhost:3306/karaoke";
+    String url = "jdbc:mysql://mysql.smartk.kinghost.net:3306/smartk";
+    // String url = "jdbc:mysql://localhost:3306/karaoke";
     //String url = "jdbc:mysql://localhost:3306/escola";
-    String usuario = "root";
-    String senha = "";                   
+    String usuario = "smartk";
+    // String usuario = "root";
+    String senha = "Ka1r60k7e";                   
+    // String senha = "Ka1r60k7e";
+    
        Connection conexao = null;
         try {
             conexao = DriverManager.getConnection(url, usuario, senha);
